ed9d84285594f61279eaff3e2ae918b227c42ee6
Simon Braillard <simon.braillard@hotmail.com>
2024-12-19 13:58:08 +0100

    updating licence

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,20 @@
+// Copyright 2024 Simon Braillard
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_new_badger/flutter_new_badger.dart';
+import 'dart:developer' as developer;
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +28,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -22,11 +36,10 @@ class _MyAppState extends State<MyApp> {
 
   void init() async {
     await FlutterNewBadger.setBadge(6);
-    print('Badge set');
+    developer.log('Badge set', name: 'FlutterNewBadger');
     await FlutterNewBadger.removeBadge();
-    print('Badge removed');
+    developer.log('Badge removed', name: 'FlutterNewBadger');
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +48,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
+        body: const Center(
           child: Text('New badger plugin'),
         ),
       ),
